be0c4ccfe71b37950452770c0c0a8c2acd696618
nyeke <nyeke@hotmail.com>
2018-06-06 17:46:56 +0200

    ejercicio9

--- src/com/cice/ejercicio6/Main.java
+++ src/com/cice/ejercicio6/Main.java
@@ -3,5 +3,14 @@ package com.cice.ejercicio6;
 public class Main {
     public static void main(String[] args) {
 
+        int[] array = {1,2,3,4,5,6,-7,8,9};
+        int recorrido = 0;
+
+        while (array[recorrido] > 0){
+            System.out.println("El valor es: " + array[recorrido]);
+            recorrido++;
+        }
+
+        System.out.println("Se han recorrido " + recorrido + " posiciones");
     }
 }
